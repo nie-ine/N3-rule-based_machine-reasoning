ad0ee21c6a2bc71d8451b17e7c1ac17774b74b49
hanscools <hanscools@intergga.ch>
2017-05-18 14:57:35 +0200

    upload OWL cardiniality rules

---
+++ owl-cardinality/cardinality_UB.cmd
@@ -0,0 +1,2 @@
+call eye --nope --traditional ...\images-demo-data.ttl ...\knora-base.ttl ...\images.ttl http://eulersharp.sourceforge.net/2003/03swap/rdfs-subClassOf http://eulersharp.sourceforge.net/2003/03swap/rdfs-subPropertyOf ...\cardinality.n3 --query cardinality_query.n3 |cwm > ...\cardinalityResult.n3
+pause
