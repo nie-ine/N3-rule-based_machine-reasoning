22445efa8d7646040a4c3de8f9afe2e05ef32285
hanscools <hanscools@intergga.ch>
2017-05-19 16:29:45 +0200

    e

--- rules_NIE_owl-cardinality/cardinality_Q.cmd
+++
@@ -1,7 +0,0 @@
-call eye --nope --traditional C:\Users\Katelijne\Documents\Hans\IT\ST\machineReasoning\N3_rules_rdfs-owl_model\owl_cardinality\images-demo-data.ttl C:\Users\Katelijne\Documents\Hans\IT\ST\machineReasoning\N3_rules_rdfs-owl_model\owl_cardinality\knora-base.ttl C:\Users\Katelijne\Documents\Hans\IT\ST\machineReasoning\N3_rules_rdfs-owl_model\owl_cardinality\images.ttl http://eulersharp.sourceforge.net/2003/03swap/owl-Nothing http://eulersharp.sourceforge.net/2003/03swap/rdfs-subClassOf http://eulersharp.sourceforge.net/2003/03swap/rdfs-subPropertyOf C:\Users\Katelijne\Documents\Hans\IT\ST\machineReasoning\N3_rules_rdfs-owl_model\owl_cardinality\cardinality.n3 --query cardinality_query.n3 |cwm > C:\Users\Katelijne\Documents\Hans\IT\ST\machineReasoning\N3_rules_rdfs-owl_model\owl_cardinality\cardinalityResult.n3
-pause
-:: --traditional
-:: |cwm
-:: --pass
-:: --query
-:: http://eulersharp.sourceforge.net/2003/03swap/owl-Nothing.n3
