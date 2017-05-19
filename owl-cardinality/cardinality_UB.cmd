bd8d4bec8eeaa56e44ab730fb8473407ada2705f
hanscools <hanscools@intergga.ch>
2017-05-20 00:18:56 +0200

    update OWL cardinality rules

--- owl-cardinality/cardinality_UB.cmd
+++ owl-cardinality/cardinality_UB.cmd
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 call eye --nope --traditional ...\images-demo-data.ttl ...\knora-base.ttl ...\images.ttl http://eulersharp.sourceforge.net/2003/03swap/rdfs-subClassOf http://eulersharp.sourceforge.net/2003/03swap/rdfs-subPropertyOf ...\cardinality.n3 --query cardinality_query.n3 |cwm > ...\cardinalityResult.n3
-=======
-call eye --nope --traditional ...\images-demo-data.ttl ...\knora-base.ttl ...\images.ttl http://eulersharp.sourceforge.net/2003/03swap/rdfs-subClassOf http://eulersharp.sourceforge.net/2003/03swap/rdfs-subPropertyOf ...\cardinality.n3 --query cardinality_query.n3 |cwm > ...\cardinalityResult.n3
->>>>>>> 01ed7acab085f8d6cf90948c411f9442c930c7df
 pause
