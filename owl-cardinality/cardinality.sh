343e523f5de14135d45552a86e787da0514eeb30
hanscools <hanscools@intergga.ch>
2017-05-20 23:07:37 +0200

    update command line for Linux and MacOS

--- owl-cardinality/cardinality.sh
+++ owl-cardinality/cardinality.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-./eye.sh --nope --traditional .../owl-cardinality/images-demo-data.ttl .../owl-cardinality/knora-base.ttl .../owl-cardinality/images.ttl http://eulersharp.sourceforge.net/2003/03swap/rdfs-subClassOf http://eulersharp.sourceforge.net/2003/03swap/rdfs-subPropertyOf .../owl-cardinality/cardinality.n3 --query .../owl-cardinality/cardinalityQuery.n3 |cwm > .../owl-cardinality/cardinalityResult.n3
+./eye.sh --nope --traditional
+.../owl-cardinality/images-demo-data.ttl
+.../owl-cardinality/knora-base.ttl
+.../owl-cardinality/images.ttl
+http://eulersharp.sourceforge.net/2003/03swap/rdfs-subClassOf
+http://eulersharp.sourceforge.net/2003/03swap/rdfs-subPropertyOf
+.../owl-cardinality/cardinality.n3
+--query .../owl-cardinality/cardinalityQuery.n3
+|cwm > .../owl-cardinality/cardinalityResult.n3
