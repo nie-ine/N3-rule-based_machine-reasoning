736227bbed6990f3f75ffed5ed7e82d9b39c3588
hanscools <hanscools@intergga.ch>
2017-05-30 12:46:05 +0200

    Update cardinality.sh

--- owl-cardinality/cardinality.sh
+++ owl-cardinality/cardinality.sh
@@ -5,7 +5,10 @@
 .../owl-cardinality/images.ttl           # Images Ontology [2] for its class restrictions on properties
 http://eulersharp.sourceforge.net/2003/03swap/rdfs-subClassOf # N3 rules expressing the RDFS sub-class model theory [3], for restriction check on different levels
 http://eulersharp.sourceforge.net/2003/03swap/rdfs-subPropertyOf # N3 rules expressing the RDFS sub-property model theory [4], for restriction check on different levels
-.../owl-cardinality/cardinality.n3 # N3 rules expressing the OWL cardinality model theory
+.../owl-cardinality/cardinality.n3       # N3 rules expressing the OWL cardinality model theory
 --query .../owl-cardinality/cardinalityQuery.n3 # The cardinality N3 rules have a boolean as conclusion, thus no need for a query or pass, but added here as example
 |cwm > .../owl-cardinality/cardinalityResult.n3  
-[1] [Knora Base Ontology](https://github.com/dhlab-basel/Knora/blob/develop/knora-ontologies/knora-base.ttl), [2] [Images Ontology](https://github.com/dhlab-basel/Knora/blob/develop/webapi/_test_data/ontologies/images-onto.ttl), [3] [rdfs-subClassOf](http://eulersharp.sourceforge.net/2003/03swap/rdfs-subClassOf), [4] [rdfs-subPropertyOf](http://eulersharp.sourceforge.net/2003/03swap/rdfs-subPropertyOf)
+[1] [Knora Base Ontology](https://github.com/dhlab-basel/Knora/blob/develop/knora-ontologies/knora-base.ttl)  
+[2] [Images Ontology](https://github.com/dhlab-basel/Knora/blob/develop/webapi/_test_data/ontologies/images-onto.ttl)  
+[3] [rdfs-subClassOf](http://eulersharp.sourceforge.net/2003/03swap/rdfs-subClassOf)  
+[4] [rdfs-subPropertyOf](http://eulersharp.sourceforge.net/2003/03swap/rdfs-subPropertyOf)
